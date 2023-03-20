cc2463f7f71eb0639e67fa8b0a842c48f3ee29d9
SHENYY1993 <shenyangyang163@163.com>
2023-03-20 10:01:31 +0800

    PathFinding GUI删除算法类

--- src/main/java/com/shenyy/pretendto/pathfactory/dijkstra2/PathFinding.java
+++ src/main/java/com/shenyy/pretendto/pathfactory/dijkstra2/PathFinding.java
@@ -54,7 +54,6 @@ public class PathFinding {
     //draw final path with line
     public List<com.shenyy.pretendto.pathfactory.algo.Node> linePath = new ArrayList<>();
 
-    Algorithm Alg = new Algorithm();
     PathFactory<Point, Obstacle<Point>> staticPathFactory;
     Random r = new Random();
     //SLIDERS
@@ -77,9 +76,9 @@ public class PathFinding {
     JLabel checkL = new JLabel("Checks: " + checks);
     JLabel lengthL = new JLabel("Path Length: " + length);
     //LABELS——ACO
-    JLabel alphaL = new JLabel("alpha");
-    JLabel betaL = new JLabel("beta");
-    JLabel rhoL = new JLabel("rho");
+    JLabel param1L = new JLabel("alpha");
+    JLabel param2L = new JLabel("beta");
+    JLabel param3L = new JLabel("rho");
     JLabel alphaValueL = new JLabel(String.valueOf(alpha));
     JLabel betaValueL = new JLabel(String.valueOf(beta));
     JLabel rhoValueL = new JLabel(String.valueOf(rho));
@@ -262,24 +261,24 @@ public class PathFinding {
         alphaSL.setMajorTickSpacing(10);
 
         space = 25;
-        alphaL.setBounds(15, space, 40, 25);
-        algoInfoP.add(alphaL);
+        param1L.setBounds(15, space, 40, 25);
+        algoInfoP.add(param1L);
         alphaSL.setMajorTickSpacing(10);
         alphaSL.setBounds(50, space, 100, 25);
         algoInfoP.add(alphaSL);
         alphaValueL.setBounds(160, space, 40, 25);
         algoInfoP.add(alphaValueL);
         space += buff;
-        betaL.setBounds(15, space, 40, 25);
-        algoInfoP.add(betaL);
+        param2L.setBounds(15, space, 40, 25);
+        algoInfoP.add(param2L);
         betaSL.setMajorTickSpacing(10);
         betaSL.setBounds(50, space, 100, 25);
         algoInfoP.add(betaSL);
         betaValueL.setBounds(160, space, 40, 25);
         algoInfoP.add(betaValueL);
         space += buff;
-        rhoL.setBounds(15, space, 40, 25);
-        algoInfoP.add(rhoL);
+        param3L.setBounds(15, space, 40, 25);
+        algoInfoP.add(param3L);
         rhoSL.setMajorTickSpacing(10);
         rhoSL.setBounds(50, space, 100, 25);
         algoInfoP.add(rhoSL);
@@ -298,8 +297,9 @@ public class PathFinding {
             @Override
             public void actionPerformed(ActionEvent e) {
                 reset();
-                if ((startx > -1 && starty > -1) && (finishx > -1 && finishy > -1))
+                if ((startx > -1 && starty > -1) && (finishx > -1 && finishy > -1)) {
                     solving = true;
+                }
             }
         });
         resetB.addActionListener(new ActionListener() {
@@ -512,15 +512,7 @@ public class PathFinding {
 
                     //绘制采样点及连线
                     if (curAlg == 3 || curAlg == 4) {
-//                        if (map[x][y].getType() == 4) {
-//                            g.setColor(Color.BLACK);
-//                            g.fillOval((int) ((x + 0.5 - 0.1) * CSIZE), (int) ((y + 0.5 - 0.1) * CSIZE), (int) (0.2 * CSIZE), (int) (0.2 * CSIZE)); //画圆点
-//                            g.setColor(Color.GREEN);
-//                            g.drawLine((int) ((map[x][y].getLastX() + 0.5) * CSIZE), (int) ((map[x][y].getLastY() + 0.5) * CSIZE), (int) ((x + 0.5) * CSIZE), (int) ((y + 0.5) * CSIZE));
-//                        } else if (map[x][y].getType() == 5) {
-//                            g.setColor(Color.RED);
-//                            g.drawLine((int) ((map[x][y].getLastX() + 0.5) * CSIZE), (int) ((map[x][y].getLastY() + 0.5) * CSIZE), (int) ((x + 0.5) * CSIZE), (int) ((y + 0.5) * CSIZE));
-//                        }
+
                     } else {
                         g.setColor(Color.BLACK);
                         g.drawRect(x * CSIZE, y * CSIZE, CSIZE, CSIZE);
@@ -542,14 +534,6 @@ public class PathFinding {
                         if (linePath.get(i).getParent() != null)
                             g.drawLine((int) ((linePath.get(i).getParent().getX()) * CSIZE), (int) ((linePath.get(i).getParent().getY()) * CSIZE), (int) ((linePath.get(i).getX()) * CSIZE), (int) ((linePath.get(i).getY()) * CSIZE));
                     }
-
-                    //DEBUG STUFF
-					/*
-					if(curAlg == 1)
-						g.drawString(map[x][y].getHops()+"/"+map[x][y].getEuclidDist(), (x*CSIZE)+(CSIZE/2)-10, (y*CSIZE)+(CSIZE/2));
-					else 
-						g.drawString(""+map[x][y].getHops(), (x*CSIZE)+(CSIZE/2), (y*CSIZE)+(CSIZE/2));
-					*/
                 }
             }
         }
@@ -626,123 +610,6 @@ public class PathFinding {
 
         @Override
         public void mouseReleased(MouseEvent e) {
-        }
-    }
-
-    class Algorithm {    //ALGORITHM CLASS
-
-        //DIJKSTRA WORKS BY PROPAGATING OUTWARDS UNTIL IT FINDS THE FINISH AND THEN WORKING ITS WAY BACK TO GET THE PATH
-        //IT USES A PRIORITY QUE TO KEEP TRACK OF NODES THAT IT NEEDS TO EXPLORE
-        //EACH NODE IN THE PRIORITY QUE IS EXPLORED AND ALL OF ITS NEIGHBORS ARE ADDED TO THE QUE
-        //ONCE A NODE IS EXLPORED IT IS DELETED FROM THE QUE
-        //AN ARRAYLIST IS USED TO REPRESENT THE PRIORITY QUE
-        //A SEPERATE ARRAYLIST IS RETURNED FROM A METHOD THAT EXPLORES A NODES NEIGHBORS
-        //THIS ARRAYLIST CONTAINS ALL THE NODES THAT WERE EXPLORED, IT IS THEN ADDED TO THE QUE
-        //A HOPS VARIABLE IN EACH NODE REPRESENTS THE NUMBER OF NODES TRAVELED FROM THE START
-        public void Dijkstra() {
-            List<Node> priority = new ArrayList<>();    //CREATE A PRIORITY QUE
-            priority.add(map[startx][starty]);    //ADD THE START TO THE QUE
-            while (solving) {
-                if (priority.size() <= 0) {    //IF THE QUE IS 0 THEN NO PATH CAN BE FOUND
-                    solving = false;
-                    break;
-                }
-                double hops = priority.get(0).getHops() + 1;    //INCREMENT THE HOPS VARIABLE
-                ArrayList<Node> explored = exploreNeighbors(priority.get(0), hops);    //CREATE AN ARRAYLIST OF NODES THAT WERE EXPLORED
-                if (explored.size() > 0) {
-                    priority.remove(0);    //REMOVE THE NODE FROM THE QUE
-                    priority.addAll(explored);    //ADD ALL THE NEW NODES TO THE QUE
-                    Update();
-                    delay();
-                } else {    //IF NO NODES WERE EXPLORED THEN JUST REMOVE THE NODE FROM THE QUE
-                    priority.remove(0);
-                }
-            }
-        }
-
-        //A STAR WORKS ESSENTIALLY THE SAME AS DIJKSTRA CREATING A PRIORITY QUE AND PROPAGATING OUTWARDS UNTIL IT FINDS THE END
-        //HOWEVER ASTAR BUILDS IN A HEURISTIC OF DISTANCE FROM ANY NODE TO THE FINISH
-        //THIS MEANS THAT NODES THAT ARE CLOSER TO THE FINISH WILL BE EXPLORED FIRST
-        //THIS HEURISTIC IS BUILT IN BY SORTING THE QUE ACCORDING TO HOPS PLUS DISTANCE UNTIL THE FINISH
-        public void AStar() {
-            ArrayList<Node> priority = new ArrayList<>();
-            priority.add(map[startx][starty]);
-            while (solving) {
-                if (priority.size() <= 0) {
-                    solving = false;
-                    break;
-                }
-                double hops = priority.get(0).getHops() + 1;
-                ArrayList<Node> explored = exploreNeighbors(priority.get(0), hops);
-                if (explored.size() > 0) {
-                    priority.remove(0);
-                    priority.addAll(explored);
-                    Update();
-                    delay();
-                } else {
-                    priority.remove(0);
-                }
-                sortQue(priority);    //SORT THE PRIORITY QUE
-            }
-        }
-
-        public ArrayList<Node> sortQue(ArrayList<Node> sort) {    //SORT PRIORITY QUE
-            int c = 0;
-            while (c < sort.size()) {
-                int sm = c;
-                for (int i = c + 1; i < sort.size(); i++) {
-                    if (sort.get(i).getEuclidDist(finishx, finishy) + sort.get(i).getHops() < sort.get(sm).getEuclidDist(finishx, finishy) + sort.get(sm).getHops())
-                        sm = i;
-                }
-                if (c != sm) {
-                    Node temp = sort.get(c);
-                    sort.set(c, sort.get(sm));
-                    sort.set(sm, temp);
-                }
-                c++;
-            }
-            return sort;
-        }
-
-        public ArrayList<Node> exploreNeighbors(Node current, double hops) {    //EXPLORE NEIGHBORS
-            ArrayList<Node> explored = new ArrayList<Node>();    //LIST OF NODES THAT HAVE BEEN EXPLORED
-            for (int a = -1; a <= 1; a++) {
-                for (int b = -1; b <= 1; b++) {
-                    int xbound = current.getX() + a;
-                    int ybound = current.getY() + b;
-                    if ((xbound > -1 && xbound < cells) && (ybound > -1 && ybound < cells)) {    //MAKES SURE THE NODE IS NOT OUTSIDE THE GRID
-                        Node neighbor = map[xbound][ybound];
-                        if ((neighbor.getHops() == -1 || neighbor.getHops() > hops) && neighbor.getType() != 2) {    //CHECKS IF THE NODE IS NOT A WALL AND THAT IT HAS NOT BEEN EXPLORED
-                            explore(neighbor, current.getX(), current.getY(), hops);    //EXPLORE THE NODE
-                            explored.add(neighbor);    //ADD THE NODE TO THE LIST
-                        }
-                    }
-                }
-            }
-            return explored;
-        }
-
-        public void explore(Node current, int lastx, int lasty, double hops) {    //EXPLORE A NODE
-            if (current.getType() != 0 && current.getType() != 1)    //CHECK THAT THE NODE IS NOT THE START OR FINISH
-                current.setType(4);    //SET IT TO EXPLORED
-            current.setLastNode(lastx, lasty);    //KEEP TRACK OF THE NODE THAT THIS NODE IS EXPLORED FROM
-            current.setHops(hops);    //SET THE HOPS FROM THE START
-            checks++;
-            if (current.getType() == 1) {    //IF THE NODE IS THE FINISH THEN BACKTRACK TO GET THE PATH
-                backtrack(current.getLastX(), current.getLastY(), hops);
-            }
-        }
-
-        public void backtrack(int lx, int ly, double hops) {    //BACKTRACK
-            length = hops;
-            while (hops > 1) {    //BACKTRACK FROM THE END OF THE PATH TO THE START
-                Node current = map[lx][ly];
-                current.setType(5);
-                lx = current.getLastX();
-                ly = current.getLastY();
-                hops--;
-            }
-            solving = false;
         }
     }
 }
